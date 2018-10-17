63955c0ba145c6d79496505822542ccbd66d91a0
antoniedoru <doru0038@stud.kea.dk>
2018-10-17 13:57:25 +0200

    menu
    need to add graff into menu 5.

---
+++ menu.py
@@ -0,0 +1,33 @@
+import os
+import time
+os.system('cls')
+def print_menu():
+    print (15*"-", "MENU", 15*"-")
+    print("1. Trial balance")
+    print("2. Balancing the accounts")
+    print("3. Profit for each month")
+    print("4. Exit")
+    print(37* "-")
+
+loop = True
+
+while loop == True:
+    print_menu()
+    choice = str(input("Enter your choice [1-4]: "))
+
+    if choice == "1":
+        os.system('cls')
+        print("Trial balance has been selected")
+    elif choice == "2":
+        os.system('cls')
+        print("Balancing the accounts has been selected")
+    elif choice == "3":
+        os.system('cls')
+        print("Profit for each month  has been selected")
+    elif choice == "4":
+        os.system('cls')
+        print("Menu  has been selected")
+        loop = False
+        os.system('cls')
+
+print("THE END")
