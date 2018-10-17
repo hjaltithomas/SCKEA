ce8950081a11c0295854dca32854ba329105a82c
hjaltithomas <hjaltith@live.com>
2018-10-17 14:52:39 +0200

    Added to menu.
    Plotteed graph

--- menu.py
+++ menu.py
@@ -6,7 +6,8 @@ def print_menu():
     print("1. Trial balance")
     print("2. Balancing the accounts")
     print("3. Profit for each month")
-    print("4. Exit")
+    print("4. Plotted profit graph")
+    print("5. Exit")
     print(37* "-")
 
 loop = True
@@ -25,6 +26,9 @@ while loop == True:
         os.system('cls')
         print("Profit for each month  has been selected")
     elif choice == "4":
+        os.system('cls')
+        print("Plotted profit graph has been selected")
+    elif choice == "5":
         os.system('cls')
         print("Menu  has been selected")
         loop = False
