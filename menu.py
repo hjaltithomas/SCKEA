083ecb06ba5e864258374336c32e61e9a875deef
antoniedoru <doru0038@stud.kea.dk>
2018-10-18 18:43:13 +0200

    menu updated

--- menu.py
+++ menu.py
@@ -1,20 +1,29 @@
 import os
 import time
 os.system('cls')
+print()
+print()
+print(10*"-","WELCOME TO FANCYUNICORN.DK",10*"-")
+print()
+print()
+print("Press Enter key to continue")
+key = input()
+if key == True:
+    os.system('cls')
 def print_menu():
     print (15*"-", "MENU", 15*"-")
     print("1. Trial balance")
     print("2. Balancing the accounts")
     print("3. Profit for each month")
-    print("4. Exit")
+    print("4. Graff")
+    print("5. Exit")
     print(37* "-")
 
 loop = True
 
 while loop == True:
     print_menu()
-    choice = str(input("Enter your choice [1-4]: "))
-
+    choice = str(input("Enter your choice [1-5]: "))
     if choice == "1":
         os.system('cls')
         print("Trial balance has been selected")
@@ -25,6 +34,9 @@ while loop == True:
         os.system('cls')
         print("Profit for each month  has been selected")
     elif choice == "4":
+        os.system('cls')
+        print("Graff has been selected")
+    elif choice == "5":
         os.system('cls')
         print("Menu  has been selected")
         loop = False
